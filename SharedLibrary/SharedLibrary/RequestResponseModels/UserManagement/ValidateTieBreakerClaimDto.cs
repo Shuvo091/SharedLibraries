00c753ba9b389ff89bb424be6e8d4a02a132edb9
Shuvo <mhshuvo@triovalinc.com>
2025-07-25 20:45:24 +0600

    Add some models

---
+++ SharedLibrary/SharedLibrary/RequestResponseModels/UserManagement/ValidateTieBreakerClaimDto.cs
@@ -0,0 +1,41 @@
+﻿namespace SharedLibrary.RequestResponseModels.UserManagement;
+
+#region Validate Tie Breaker Claim Request
+public class ValidateTiebreakerClaimRequest
+{
+	public string OriginalJobId { get; set; } = default!;
+	public string TiebreakerRequestId { get; set; } = default!;
+	public string Urgency { get; set; } = default!;
+	public DateTime TiebreakerDeadline { get; set; }
+	public double BonusMultiplier { get; set; }
+	public int RequiredMinElo { get; set; }
+	public List<Guid> OriginalTranscribers { get; set; } = new();
+}
+
+
+#endregion
+
+
+#region Validate Tie Breaker Claim Response
+
+public class ValidateTiebreakerClaimResponse
+{
+	public bool TiebreakerClaimValidated { get; set; }
+	public Guid UserId { get; set; } = default!;
+	public bool UserEloQualified { get; set; }
+	public int CurrentElo { get; set; }
+	public bool IsOriginalTranscriber { get; set; }
+	public string ClaimId { get; set; } = default!;
+	public bool BonusConfirmed { get; set; }
+	public string EstimatedCompletion { get; set; } = default!; // e.g. "45m"
+}
+
+public class TiebreakerClaimForbiddenResponse
+{
+	public string Error { get; set; } = "original_transcriber_not_eligible";
+	public string Message { get; set; } = "Users who participated in the original transcription cannot be tiebreakers";
+	public string UserId { get; set; } = default!;
+	public bool WasOriginalTranscriber { get; set; }
+}
+
+#endregion
---
+++ SharedLibrary/SharedLibrary/RequestResponseModels/UserManagement/VerificationRequirementDto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.RequestResponseModels.UserManagement;
+
+#region Verification Requirement Request
+
+public class UpdateVerificationRequirementsRequest
+{
+	public bool RequireIdDocument { get; set; }
+	public bool RequirePhotoUpload { get; set; }
+	public bool RequirePhoneVerification { get; set; }
+	public bool RequireEmailVerification { get; set; }
+	public string VerificationLevel { get; set; } = default!;
+	public Dictionary<string, string> ValidationRules { get; set; } = [];
+	public string Reason { get; set; } = default!;
+}
+
+#endregion
+
+#region Verification Requirement Response
+
+public class UpdateVerificationRequirementsResponse
+{
+	public bool RequirementsUpdated { get; set; }
+	public bool PiiDataCollection { get; set; }
+	public string ComplianceMode { get; set; } = default!;
+	public string VerificationLevel { get; set; } = default!;
+	public List<string> VerificationSteps { get; set; } = [];
+	public Dictionary<string, string> RoadmapEnhancements { get; set; } = [];
+}
+
+#endregion
