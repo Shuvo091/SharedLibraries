77642f33fd4e9ffe45d92d06a08b919d99401d66
PiasRoY <pias.kuet@gmail.com>
2025-07-29 03:46:59 +0600

    professional spot check models

---
+++ SharedLibrary/SharedLibrary/RequestResponseModels/QA/ProfessionalSpotCheckConfig/ProfessionalSpotCheckRequest.cs
@@ -0,0 +1,23 @@
+﻿namespace SharedLibrary.RequestResponseModels.QA.ProfessionalSpotCheckConfig;
+
+public class ProfessionalSpotCheckRequest
+{
+    public Configuration? Configuration { get; set; }
+    public ProfessionalFilters? ProfessionalFilters { get; set; }
+}
+
+public class Configuration
+{
+    public double SpotCheckPercentage { get; set; }
+    public double MinimumSampleSize { get; set; }
+    public double MaxChecksPerDay { get; set; }
+    public double QualityThreshold { get; set; }
+    public bool EnableRandomSampling { get; set; }
+}
+
+public class ProfessionalFilters
+{
+    public double MinEloRating { get; set; }
+    public List<string>? IncludeDialects { get; set; }
+    public string? TimeRange { get; set; }
+}
