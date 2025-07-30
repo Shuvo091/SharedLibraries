99738e4ffa3fa17cb66efbcc959a4844b824c979
pranto1209 <masumpranto12@gmail.com>
2025-07-30 22:58:23 +0600

    Update Transcription Model

--- SharedLibrary/SharedLibrary/RequestResponseModels/Transcription/Requests/CreateJobAssignRequest.cs
+++ SharedLibrary/SharedLibrary/RequestResponseModels/Transcription/Requests/CreateJobAssignRequest.cs
@@ -3,7 +3,7 @@
     public class CreateJobAssignRequest
     {
         public Guid? WorkflowRequestId { get; set; }
-        public Guid? UserId { get; set; }
+        public Guid UserId { get; set; }
         public Guid ClaimId { get; set; }
         public int? TranscriptionSequence { get; set; }
         public string? AssignedBy { get; set; }
--- SharedLibrary/SharedLibrary/RequestResponseModels/Transcription/Responses/GetComparisonReadinessResponse.cs
+++ SharedLibrary/SharedLibrary/RequestResponseModels/Transcription/Responses/GetComparisonReadinessResponse.cs
@@ -10,14 +10,14 @@
         public string? WaitingFor { get; set; }
         public DateTime? EstimatedReadiness { get; set; }
         public bool? ProfessionalBypass { get; set; }
-        public string? ProfessionalTranscriber { get; set; }
+        public Guid? ProfessionalTranscriber { get; set; }
         public string? NextAction { get; set; }
     }
 
     public class ComparisonReadinessTranscriptionResponse
     {
-        public string? TranscriptionId { get; set; }
-        public string? TranscriberId { get; set; }
+        public Guid? TranscriptionId { get; set; }
+        public Guid? TranscriberId { get; set; }
         public int? Sequence { get; set; }
         public DateTime? CompletedAt { get; set; }
         public bool? IsProfessional { get; set; }
