42f4c104e0eb09d0ba49cf5adabbeff35c531b48
Shuvo <mhshuvo@triovalinc.com>
2025-08-02 17:52:24 +0600

    Add new DTO

---
+++ SharedLibrary/SharedLibrary/RequestResponseModels/UserManagement/ProfessionalStatusBatchResponseDto.cs
@@ -0,0 +1,20 @@
+﻿namespace SharedLibrary.RequestResponseModels.UserManagement;
+
+public class ProfessionalStatusBatchResponse
+{
+	public Dictionary<string, ProfessionalStatus> ProfessionalStatuses { get; set; } = new Dictionary<string, ProfessionalStatus>();
+	public ProfessionalSummary Summary { get; set; } = new ProfessionalSummary();
+}
+
+public class ProfessionalStatus
+{
+	public bool IsProfessional { get; set; }
+	public bool BypassQAComparison { get; set; }
+}
+
+public class ProfessionalSummary
+{
+	public int TotalChecked { get; set; }
+	public int Professionals { get; set; }
+	public int StandardTranscribers { get; set; }
+}
