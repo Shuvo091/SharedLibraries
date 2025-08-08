fe80db56e96968e7b73b6984f6edf760c9c8af2f
Shuvo <mhshuvo@triovalinc.com>
2025-08-08 21:25:07 +0600

    Add tie breaker and 3rd transcriber req and resp

---
+++ SharedLibrary/SharedLibrary/Contracts/Transcription/Responses/RequestAdditionalTranscriberResponse.cs
@@ -0,0 +1,29 @@
+﻿namespace SharedLibrary.Contracts.Transcription.Responses;
+
+/// <summary>
+/// Response object for additional transcriber request.
+/// </summary>
+public class RequestAdditionalTranscriberResponse
+{
+    /// <summary>
+    /// Gets or sets a value indicating whether additonal transcriber was requested.
+    /// </summary>
+    public bool AdditionalTranscriberRequested { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating new job status.
+    /// Gets kept as string because incoming values use snake_case (e.g. "seeking_additional_transcriber").
+    /// </summary>
+    public string JobStatus { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets value indicating estimated time match.
+    /// Free-form estimated match time (e.g. "30m"). You can parse it to TimeSpan if you standardize format.
+    /// </summary>
+    public string EstimatedMatchTime { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets value indicating extended dealine.
+    /// </summary>
+    public DateTime ExtendedDeadline { get; set; }
+}
